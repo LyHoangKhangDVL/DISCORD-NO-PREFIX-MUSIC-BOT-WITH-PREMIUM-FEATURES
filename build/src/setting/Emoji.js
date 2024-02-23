9e8982352e083db60cdfa47b881e84d85dcc2164
TheExtremezCoder <156786126+TheExtremezCoder@users.noreply.github.com>
2024-02-23 22:44:48 +0530

    Add files via upload

---
+++ build/src/setting/Emoji.js
@@ -0,0 +1,139 @@
+export default class OrasEmoji extends Object {
+  constructor(client) {
+    super();
+    this.tick = "✅";
+    this.cross = "❌";
+    this.playing = "🎵";
+    this.exclamation = "❗";
+    this.queue = "📜";
+    this.info = "❗";
+    this.defSearch = "▶";
+    this.premium = "💎";
+    this.invite = "🔗";
+    this.support = "🖇️";
+    this.spotiSearch = "🪇";
+    this.deezSearch = "🎼";
+    this.vote = "🗳️";
+    this.soundSearch = "☁️";
+    this.badges = {
+      named: "👑",
+      owner: "👑",
+      dev: "`</>`",
+      admin: "☄️",
+      codev: "🛠️",
+      author: "",
+      friend: "<:bsdk_friends:1169758132260585613>",
+      vip: "<:dc_Vipz:1169758685128577085>",
+      premiumUser: "💎",
+      mod: "<:mod_badge:1166375421986750564>",
+      staff: "<:Staff:1166375424486539336>",
+      supporter: "<:earlysupporter:1166375426956996619>",
+      user: "<:kronix_member:1075040201195393074>",
+    };
+    this.helpMenu = {
+      music: "🎵",
+      home: "<:Home:1166377282726797323>",
+      filters: "<:icons_control:1166377285499224164>",
+      info: "❗",
+      utility: "<:settings:1166377288171004054>",
+      allCommands: "<:icons_richpresence:1166377290662432809>",
+    };
+    this.setup = {
+      pause: "<:ss_pause:1099927332204073011>",
+      resume: "<:resume:1099927448734408744>",
+      skip: "<:forward10:986893749005217812>",
+      previous: "<:Oras_Previous:1137298056283430982>",
+      shuffle: "<:shuffle:1139766210213462066>",
+      author: "<:icons_queue:1166374069717958666>",
+      nowPlaying: "<:kronix_member:1075040201195393074>",
+      requester: "<:kronix_member:1075040201195393074>",
+      duration: "<:duration333:1169749340923904060>",
+      stop: "<:ss_stop:1099927856651436082>",
+      loop: "<:loop:1129823572991426651>",
+      volLow: "<:lower_vol:1139766602527690902>",
+      volHigh: "<:higher_vol:1139766719917854751>",
+      fav: "<:discotoolsxyzicon8:1158301800311566398>",
+      autoplay: "<:emoji_05:1158269184715788388>",
+      filters: "<:icons_control:1166377285499224164>",
+    };
+    this.orasNew = {
+      emote: "🎵",
+      nowPlaying: "🎵",
+      requester: "<:kronix_member:1075040201195393074>",
+      duration: "<:duration333:1169749340923904060>",
+      author: "<:icons_queue:1166374069717958666>",
+      pause: "<:Pause:1129824564386467991>",
+      resume: "<:resume:1129824021618380801>",
+      skip: "<:forward10:986893749005217812>",
+      fav: "💎",
+      previous: "<:Oras_Previous:1137298056283430982>",
+      stop: "<:stop:1129823823399751690>",
+    };
+    this.spotify = {
+      emote: "🎵",
+      filters: "",
+      nowPlaying: "🪇",
+      requester: "<:kronix_member:1075040201195393074>",
+      duration: "<:duration333:1169749340923904060>",
+      pause: "<:ss_pause:1099927332204073011>",
+      author: "<:icons_queue:1166374069717958666>",
+      resume: "<:resume:1099927448734408744>",
+      stop: "<:ss_stop:1099927856651436082>",
+      loop: "<:loop:1129823572991426651>",
+      shuffle: "<:shuffle:1139766210213462066>",
+      forward: "<:backward:1129823401175949322>",
+      backward: "<:forward10:1129823372008759327>",
+      volLow: "<:lower_vol:1139766602527690902>",
+      volHigh: "<:higher_vol:1139766719917854751>",
+      previous: "<:Oras_Previous:1137298056283430982>",
+      skip: "<:forward10:986893749005217812>",
+    };
+    this.special = {
+      emote: "🎵",
+      nowPlaying: "🎵",
+      requester: "<:kronix_member:1075040201195393074>",
+      duration: "<:duration333:1169749340923904060>",
+      pause: "<:ss_pause:1099927332204073011>",
+      author: "<:kronix_member:1075040201195393074>",
+      resume: "<:resume:1099927448734408744>",
+      stop: "<:ss_stop:1099927856651436082>",
+      loop: "<:loop:1129823572991426651>",
+      shuffle: "<:shuffle:1139766210213462066>",
+      forward: "<:backward:1129823401175949322>",
+      backward: "<:forward10:1129823372008759327>",
+      volLow: "<:lower_vol:1139766602527690902>",
+      volHigh: "<:higher_vol:1139766719917854751>",
+      previous: "<:Oras_Previous:1137298056283430982>",
+      skip: "<:last:1139768093766336632>",
+    };
+    this.noButtons = {
+      emote: "🎵",
+      nowPlaying: "<:kronix_member:1075040201195393074>",
+      author: "<:icons_queue:1166374069717958666>",
+      requester: "<:kronix_member:1075040201195393074>",
+      duration: "<:duration333:1169749340923904060>",
+      filters: "",
+    };
+    this.simple = {
+      emote: "🎵",
+      nowPlaying: "<:kronix_member:1075040201195393074>",
+      requester: "<:kronix_member:1075040201195393074>",
+      author: "<:icons_queue:1166374069717958666>",
+      duration: "<:duration333:1169749340923904060>",
+      filters: "",
+      pause: "<:ss_pause:1099927332204073011>",
+      resume: "<:resume:1099927448734408744>",
+      stop: "<:stop:1129823823399751690>",
+      skip: "<:skip:1099927755342233720>",
+      loop: "<:loop:1129823572991426651>",
+    };
+    this.oldStyle = {
+      emote: "🎵",
+      nowPlaying: "<:kronix_member:1075040201195393074>",
+      author: "<:icons_queue:1166374069717958666>",
+      requester: "<:kronix_member:1075040201195393074>",
+      duration: "<:duration333:1169749340923904060>",
+    };
+  }
+}
+//# sourceMappingURL=Emoji.js.map
